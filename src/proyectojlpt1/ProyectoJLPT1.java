b68d8e5bb27015d7f2ad16d2ca3994f16dd8cc63
T <T@PC266>
2014-09-15 19:37:03 -0500

    Mi primer upload a github

--- src/proyectojlpt1/ProyectoJLPT1.java
+++ src/proyectojlpt1/ProyectoJLPT1.java
@@ -16,7 +16,7 @@ public class ProyectoJLPT1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("mi primer programa feo");
+        System.out.println("miiiiiiiiiii primer programa feo");
     }
     
 }
